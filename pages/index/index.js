e84294b2f7f17ee8a33863fbf0c352ac9c4c6c21
yangjb <yangjb@lt51.cn>
2020-05-25 23:47:13 +0800

    首页增加商品模块

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,11 +4,15 @@ const app = getApp()
 
 Page({
   data: {
+    goodList:[{},{},{}], // 商品列表
+    hotGoodList:[{},{},{},{}],
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    bannerImgList:['https://7072-prod-e3wgf-1302240362.tcb.qcloud.la/banner1.jpg?sign=e92dc7ebb88724852e80bf4f51bb5c64&t=1590393990','https://7072-prod-e3wgf-1302240362.tcb.qcloud.la/banner2.jpg?sign=e06a3abc43bc983552578ace6cef6675&t=1590394100','https://7072-prod-e3wgf-1302240362.tcb.qcloud.la/banner3.jpg?sign=1473dd5c8fb5740221094b321db4b908&t=1590394250']
+    bannerImgList:['https://7072-prod-e3wgf-1302240362.tcb.qcloud.la/banner1.jpg?sign=e92dc7ebb88724852e80bf4f51bb5c64&t=1590393990',
+    'https://7072-prod-e3wgf-1302240362.tcb.qcloud.la/banner2.jpg?sign=e06a3abc43bc983552578ace6cef6675&t=1590394100',
+    'https://7072-prod-e3wgf-1302240362.tcb.qcloud.la/banner3.jpg?sign=1473dd5c8fb5740221094b321db4b908&t=1590394250']
   },
 
   onLoad: function () {
