b6d81de063f5a5a043de5c9b10f8ba2767290d88
yangjb <yangjb@lt51.cn>
2020-05-28 07:44:29 +0800

    商品央视调整

--- packageA/good_list/list.js
+++ packageA/good_list/list.js
@@ -1,5 +1,37 @@
 Page({
     onLoad(option){
         console.log(option,'options')
+        this.setData({
+            value:option.keyword
+        })
+    },
+    data:{
+        sortList:[{text:'默认'},{text:'人气'},{text:'销量'}],
+        value:'',
+        activeNumber:0
     }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
