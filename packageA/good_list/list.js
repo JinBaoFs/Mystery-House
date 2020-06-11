c82a54e7573e82a5657f3487ae8a03575c059c84
yangjb <yangjb@lt51.cn>
2020-06-12 00:37:35 +0800

    商品列表页样式调整

--- packageA/good_list/list.js
+++ packageA/good_list/list.js
@@ -8,7 +8,8 @@ Page({
     data:{
         sortList:[{text:'默认'},{text:'人气'},{text:'销量'}],
         value:'',
-        activeNumber:0
+        activeNumber:0,
+        imgUrl:'https://7072-prod-e3wgf-1302240362.tcb.qcloud.la/banner2.jpg?sign=e06a3abc43bc983552578ace6cef6675&t=1590394100'
     }
 })
 
