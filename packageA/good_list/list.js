9d5e1328036b44b7679327181728cde2047027d9
yangjb <yangjb@lt51.cn>
2020-06-19 15:50:32 +0800

    列表样式调整

--- packageA/good_list/list.js
+++ packageA/good_list/list.js
@@ -2,14 +2,23 @@ Page({
     onLoad(option){
         console.log(option,'options')
         this.setData({
-            value:option.keyword
+            value:option.keyword,
         })
     },
     data:{
-        sortList:[{text:'默认'},{text:'人气'},{text:'销量'}],
+        sortList:[{text:'默认'},{text:'人气'},{text:'销量'},{text:'推荐'}],
         value:'',
         activeNumber:0,
-        imgUrl:'https://7072-prod-e3wgf-1302240362.tcb.qcloud.la/banner2.jpg?sign=e06a3abc43bc983552578ace6cef6675&t=1590394100'
+        imgUrl:'https://7072-prod-e3wgf-1302240362.tcb.qcloud.la/banner2.jpg?sign=e06a3abc43bc983552578ace6cef6675&t=1590394100',
+        dataList:[{},{},{},{},{},{},{},{},{},{},{}]
+    },
+    handleChangeTab(e){
+        let _data = this.data
+        let {idx,item} = e.currentTarget.dataset
+        if(_data.activeNumber == idx)return
+        this.setData({
+            activeNumber:idx
+        })
     }
 })
 
